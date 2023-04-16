4830af3b9c5cb2693dda7fc23f1ec38fd8dd1c6f
Asonwa Nnabeze <lilynnabeze@gmail.com>
2023-04-16 21:18:53 +0100

    used axios to fetch the data

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,32 +1,54 @@
 import { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
+import axios from "axios"
+
 
 function Body() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // useEffect(() => {
+  //   fetch(`https://swapi.dev/api/films`)
+  //     .then((response) => {
+  //       // if (!response.ok) {
+  //       //   throw new Error(`Page not found: Error ${response.status}`);
+  //       // }
+  //       // console.log(response.json() )
+  //       console.log(response)
+  //       return response.json();
+  //     })
+  //     .then((actualData) => {
+  //       setData(actualData);
+  //       // setError(null);
+  //     })
+  //     .catch((error) => {
+  //       setError(error.message);
+  //       setData(null);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // }, []);
+
   useEffect(() => {
-    fetch(`https://swapi.dev/api/films`)
-      .then((response) => {
-        // if (!response.ok) {
-        //   throw new Error(`Page not found: Error ${response.status}`);
-        // }
-        // console.log(response.json() )
-        return response.json();
-      })
-      .then((actualData) => {
-        setData(actualData);
+    const getData = async () => {
+      try {
+        const response = await axios.get(`https://swapi.dev/api/films`);
+        setData(response.data);
         // setError(null);
-      })
-      .catch((error) => {
-        setError(error.message);
+      } catch (err) {
+        setError(err.message);
         setData(null);
-      })
-      .finally(() => {
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+    getData();
   }, []);
+  
+
+
 console.log(data, 'data')
   return (
     <div>
