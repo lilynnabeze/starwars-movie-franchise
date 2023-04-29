4b98fe966a3b0e0ff6aa49c6d1a796eb8e85db41
Asonwa Nnabeze <lilynnabeze@gmail.com>
2023-04-29 13:23:12 +0100

    updated the movie card background images

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,47 @@
 import React from "react";
 import "../styles/MovieCard.css";
+import bgCard1 from "../assets/bgCard1.0.png";
+import bgCard2 from "../assets/bgCard2.0.png";
+import bgCard3 from "../assets/bgCard3.0.png";
+import bgCard4 from "../assets/bgCard4.0.png";
+import bgCard5 from "../assets/bgCard5.0.png";
+import bgCard6 from "../assets/bgCard6.0.png";
+
+// const backgroundImage = [
+//   { image: bgCard1 },
+//   { image: bgCard2 },
+//   { image: bgCard3 },
+//   { image: bgCard4 },
+//   { image: bgCard5 },
+//   { image: bgCard6 },
+// ];
+// const backgroundImage = () => {
+//   if (data?.results?.episode_id === 4) {
+//     return className={bg_0}
+//   } else if (data?.results?.episode_id === 5) {
+//     return className={bg_1}
+//   } else if (data?.results?.episode_id === 6) {
+//     return className={bg_2}
+//   }
+// }
+
+// const backgroundImage = data?.results?.map(({ episode_id }) => {
+//   if (episode_id === 4) {
+//     return (className = { bg_0 });
+//   } else if (episode_id === 5) {
+//     return (className = { bg_1 });
+//   } else if (episode_id === 6) {
+//     return (className = { bg_2 });
+//   }
+// });
 
 function MovieCard({ data }) {
   console.log(data);
+
   const CardItem = data?.results?.map(
-    ({ episode_id, title, release_date, opening_crawl }) => {
+    ({ episode_id, title, release_date, opening_crawl }, index) => {
       return (
-        <li key={episode_id} className="movie-card">
+        <li key={episode_id} className={`movie-card bg_${index}`}>
           <div className="movie-card-heading">
             <h3 className="title">{title}</h3>
             <p className="release-date">
